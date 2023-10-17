247cf76cacb9670aaafdf7fd3fc348219ee7f06d
bvddw <galaid.arseniy@gmail.com>
2023-10-17 11:37:13 +0300

    menu, func prototypes and delete void added

--- lesson6/binary_trees.cpp
+++ lesson6/binary_trees.cpp
@@ -9,6 +9,13 @@ struct binary_tree {
 using namespace std;
 typedef binary_tree* Node;
 
+Node formTree(); // function to forming binary tree
+int findNodes(Node node, int info_to_comp); // function to count number of elements with chosen information in binary tree
+void directOrder(Node node); // function to print binary tree in direct order
+void symmetricalOrder(Node node); // function to print binary tree in symmetrical order
+void reversedOrder(Node node); // function to print binary tree in reversed order
+void deleteBinaryTree(Node node); // function to delete binary tree from memery
+
 Node formTree() {
     Node new_node;
     int info;
@@ -61,17 +68,62 @@ void reversedOrder(Node node) {
     }
 }
 
+void deleteBinaryTree(Node node) {
+    if (node) {
+        deleteBinaryTree(node->left);
+        deleteBinaryTree(node->right);
+        delete node;
+    }
+}
+
 int main() {
     Node main_node;
+    cout << "Please, form your binary tree." << endl;
     main_node = formTree();
-    cout << "In your tree " << findNodes(main_node, 100) << " items with value 100." << endl;
-    cout << "In direct order: ";
-    directOrder(main_node);
-    cout << endl;
-    cout << "In symmetrical order: ";
-    symmetricalOrder(main_node);
-    cout << endl;
-    cout << "In reversed order: ";
-    reversedOrder(main_node);
+    bool flag = true;
+    int choice;
+    int info_to_find;
+    while (flag) {
+        cout << "MENU" << endl;
+        cout << "1. Find Nodes with chosen info" << endl;
+        cout << "2. Output binary tree in direct order" << endl;
+        cout << "3. Output binary tree in symmetracal order" << endl;
+        cout << "4. Output binary tree in reversed order" << endl;
+        cout << "5. Finish program" << endl;
+        cin >> choice;
+        switch (choice)
+        {
+        case 1:
+            cout << "Enter info, nodes with which one you want search: ";
+            cin >> info_to_find;
+            cout << "In your tree " << findNodes(main_node, info_to_find) << " items with value " << info_to_find << "." << endl;
+            break;
+        case 2:
+            cout << "Binary tree in direct order: ";
+            directOrder(main_node);
+            cout << endl;
+            break;
+        case 3:
+            cout << "In symmetrical order: ";
+            symmetricalOrder(main_node);
+            cout << endl;
+            break;
+        case 4:
+            cout << "In reversed order: ";
+            reversedOrder(main_node);
+            cout << endl;
+            break;
+        case 5:
+            deleteBinaryTree(main_node);
+            cout << "Binary tree deleted successfully!" << endl;
+            cout << "Program finished successfully!";
+            flag = false;
+            break;
+        default:
+            cout << "Invalid choice." << endl;
+            break;
+        }
+    }
+    
     return 0;
 }
