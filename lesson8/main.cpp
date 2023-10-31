8831aafa53df48526b003c9f62320d22fc5b5665
bvddw <galaid.arseniy@gmail.com>
2023-10-31 12:30:50 +0200

    creating tree by given preorder view of expression

---
+++ lesson8/main.cpp
@@ -0,0 +1,109 @@
+#include <iostream>
+#include <stack>
+#include <cctype>
+
+using namespace std;
+
+struct Node {
+    char data;
+    Node* left;
+    Node* right;
+
+    Node(char val) : data(val), left(nullptr), right(nullptr) {}
+};
+
+bool isOperator(char c) {
+    return (c == '+' || c == '-' || c == '*');
+}
+
+bool isNum(char c) {
+    return (c == '0' || c == '1' || c == '2' || c == '3' || c == '4' || c == '5' || c == '6' || c == '7' || c == '8' || c == '9');
+}
+
+int charToInt(char c) {
+    if (c == '0') {
+        return 0;
+    }
+    if (c == '1') {
+        return 1;
+    }
+    if (c == '2') {
+        return 2;
+    }
+    if (c == '3') {
+        return 3;
+    }
+    if (c == '4') {
+        return 4;
+    }
+    if (c == '5') {
+        return 5;
+    }
+    if (c == '7') {
+        return 7;
+    }
+    if (c == '8') {
+        return 8;
+    }
+    return 9;
+}
+
+Node* buildExpressionTree(const char* expression, int& index) {
+    char c = expression[index];
+    index++;
+
+    if (c == '\0') {
+        return nullptr;
+    }
+
+    if (isNum(c)) {
+        return new Node(c);
+    } else if (isOperator(c)) {
+        Node* newNode = new Node(c);
+        newNode->left = buildExpressionTree(expression, index);
+        newNode->right = buildExpressionTree(expression, index);
+        return newNode;
+    }
+
+    return nullptr;
+}
+
+void inOrderTraversal(Node* root) {
+    if (root) {
+        if (isOperator(root->data))
+            cout << "(";
+        inOrderTraversal(root->left);
+        cout << root->data;
+        inOrderTraversal(root->right);
+        if (isOperator(root->data))
+            cout << ")";
+    }
+}
+
+double countResult(Node* root) {
+    int result = 0;
+    if (!root->left && !root->right) {
+        return charToInt(root->data);
+    }
+    if (root->data == '*') {
+        return countResult(root->left) * countResult(root->right);
+    }
+    if (root->data == '-') {
+        return countResult(root->left) - countResult(root->right);
+    }
+    return countResult(root->left) + countResult(root->right);
+}
+
+int main() {
+    char* input = new char;
+    cout << "Enter expression: ";
+    cin >> input;
+    const char* expression = input;
+    int index = 0;
+    Node* root = buildExpressionTree(expression, index);
+    cout << "Inorder traversal of the expression tree:" << endl;
+    inOrderTraversal(root);
+    cout << endl;
+    cout << "Result of the expression: " << countResult(root);
+    return 0;
+}
